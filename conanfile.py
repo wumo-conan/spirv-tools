53862938fc5c4186e6871be91e20972339419c11
wumo <wumo@outlook.com>
2020-06-12 15:36:08 +0800

    release plz

--- conanfile.py
+++ conanfile.py
@@ -48,7 +48,8 @@ class SpirvToolsConan(ConanFile):
     def package(self):
         cmake = self.configure_cmake()
         cmake.install()
-        # tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
     
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "effcee"
